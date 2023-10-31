6b2512117c2ad955da962954c9c172ac49468ee0
German <tokyo34ghoul@gmail.com>
2023-10-31 16:40:13 +0300

    xr ik apply

--- Assets/Scripts/XRPlayer.cs
+++ Assets/Scripts/XRPlayer.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using RootMotion.FinalIK;
-using Unity.Mathematics;
+﻿using RootMotion.FinalIK;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Spherum.Player
@@ -14,9 +13,9 @@ namespace Spherum.Player
 
         [Header("Input")] [SerializeField] private ActionBasedController _leftController;
         [SerializeField] private ActionBasedController _rightController;
+        [SerializeField] private InputActionProperty _inputCalibarate;
 
-
-        [SerializeField] private Transform _targetLeft;
+        [Header("IK")] [SerializeField] private Transform _targetLeft;
         [SerializeField] private Transform _targetRight;
 
 
@@ -37,11 +36,6 @@ namespace Spherum.Player
         {
             _targetLeft.transform.position = _leftController.transform.position;
             _targetRight.transform.position = _rightController.transform.position;
-            
-           //  var sizeF = (_vrIk.solver.spine.headTarget.position.y - _vrIk.references.root.position.y) /
-           //              (_vrIk.references.head.position.y - _vrIk.references.root.position.y);
-           //  
-           // _vrIk.references.root.localScale *= sizeF * _scaleMlp;
         }
 
         private void Update()
@@ -50,6 +44,15 @@ namespace Spherum.Player
             _animator.SetFloat(_hashGripRight, _rightController.selectActionValue.action.ReadValue<float>());
             _animator.SetFloat(_hashTriggerLeft, _leftController.activateActionValue.action.ReadValue<float>());
             _animator.SetFloat(_hashTriggerRight, _rightController.activateActionValue.action.ReadValue<float>());
+
+
+            if (_inputCalibarate.action.WasPressedThisFrame())
+            {
+                var sizeF = (_vrIk.solver.spine.headTarget.position.y - _vrIk.references.root.position.y) /
+                            (_vrIk.references.head.position.y - _vrIk.references.root.position.y);
+
+                _vrIk.references.root.localScale *= sizeF * _scaleMlp;
+            }
         }
     }
 }
