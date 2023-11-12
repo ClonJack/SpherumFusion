0ba8f9d65cbf2e5cdbcdd2872f3c6adcb27a0d17
German <tokyo34ghoul@gmail.com>
2023-11-12 15:31:14 +0300

    custom avatar builder

---
+++ Assets/Scripts/Editor/HumanoidAvatarBuilderEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UtilsSpherum;
+
+namespace Editor
+{
+    public class HumanoidAvatarBuilderEditor : EditorWindow
+    {
+        [MenuItem("Tools/Avatar Tools")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<HumanoidAvatarBuilderEditor>();
+            
+            window.titleContent = new GUIContent("Avatar Tools");
+            
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            if (!GUILayout.Button("Build Avatar")) return;
+            
+            var select = Selection.activeGameObject;
+                
+            var description = AvatarBuilderUtils.CreateHumanDescription(select);
+			
+            var avatar = AvatarBuilder.BuildHumanAvatar(select, description);
+            avatar.name = select.name;
+                
+            var animator = select.GetComponent<Animator>();
+            animator.avatar = avatar;
+        }
+    }
+}
---
+++ Assets/Scripts/HumanoidAvatarBuilder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UtilsSpherum
+{
+	public class HumanoidAvatarBuilder : MonoBehaviour
+	{
+		private Animator _animator;
+
+		public void Awake()
+		{
+			_animator = GetComponent<Animator>();
+			
+			var description = AvatarBuilderUtils.CreateHumanDescription(gameObject);
+			
+			var avatar = AvatarBuilder.BuildHumanAvatar(gameObject, description);
+			
+			avatar.name = gameObject.name;
+			
+			_animator.avatar = avatar;
+		}
+	}
+}
